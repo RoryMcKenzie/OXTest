0ad9bf01aa04586cc6ff3505f9bfb3ad6d0d4b76
RoryMcKenzie <rorymckenzie212@gmail.com>
2021-10-14 15:12:40 +0100

    Added randomisation to crossover algorithm and generation of parents, prints chromosomes now

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,26 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Integer> parent1 = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
+        ArrayList<Integer> parent2 = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
 
-        ArrayList<Integer> parent1 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
-        ArrayList<Integer> parent2 = new ArrayList<>(Arrays.asList(10,9,8,7,6,5,4,3,2,1));
+        Collections.shuffle(parent1);
+        Collections.shuffle(parent2);
 
         int size = parent1.size();
 
-        //To be replaced with random numbers in the future
-        int sliceStart = 3;
-        int sliceEnd = 7;
+        Random random = new Random();
+
+        int num1 = random.nextInt(size-1);
+        int num2 = random.nextInt(size);
+
+        int sliceStart = Math.min(num1,num2);
+        int sliceEnd = Math.max(num1,num2);
 
         ArrayList<Integer> child1 = new ArrayList<>();
         ArrayList<Integer> child2 = new ArrayList<>();
@@ -43,5 +50,26 @@ public class Main {
 
         Collections.rotate(child1,sliceStart);
         Collections.rotate(child2,sliceStart);
+
+        System.out.println("sliced between " + (sliceStart + 1) + " and " + sliceEnd);
+
+        System.out.println("parents: ");
+        printChromosome(parent1);
+        System.out.println("");
+        printChromosome(parent2);
+
+        System.out.println("\n");
+        System.out.println("children: ");
+        printChromosome(child1);
+        System.out.println("");
+        printChromosome(child2);
+    }
+
+    public static void printChromosome(ArrayList<Integer> chromosome){
+        for (int allele : chromosome){
+            System.out.print(Integer.toString(allele));
+        }
     }
 }
+
+
